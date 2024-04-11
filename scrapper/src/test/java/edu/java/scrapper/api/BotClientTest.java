ad1de0b62c2566172e62ea18de33606cf64cd6c9
Konstantin Zhuravlev <113041604+constZhur@users.noreply.github.com>
2024-04-11 19:30:58 +0300

    Refactored retry init and updated web-client tests

--- scrapper/src/test/java/edu/java/scrapper/api/BotClientTest.java
+++ scrapper/src/test/java/edu/java/scrapper/api/BotClientTest.java
@@ -3,20 +3,33 @@ package edu.java.scrapper.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import edu.java.clients.impl.BotClient;
+import edu.java.clients.retry.RetryConfigProxy;
+import edu.java.clients.retry.RetryPolicy;
+import edu.java.configuration.RetryConfiguration;
 import edu.java.dto.request.LinkUpdateRequest;
+import io.github.resilience4j.retry.Retry;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import java.io.IOException;
 import java.net.URI;
+import java.util.Arrays;
 import java.util.List;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BotClientTest {
+
+    private static Retry retry;
+
     private WireMockServer wireMockServer;
     private BotClient botClient;
     private ObjectMapper objectMapper;
@@ -28,11 +41,31 @@ public class BotClientTest {
         List.of(2L, 3L)
     );
 
+    @BeforeAll
+    static void beforeAll() {
+        retry = RetryConfiguration.start(RetryConfigProxy
+            .builder()
+            .policy(RetryPolicy.LINEAR)
+            .maxRetries(10)
+            .retryDelay(15L)
+            .increment(2)
+            .httpStatuses(
+                Arrays.asList(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    HttpStatus.SERVICE_UNAVAILABLE,
+                    HttpStatus.BAD_GATEWAY,
+                    HttpStatus.GATEWAY_TIMEOUT,
+                    HttpStatus.INSUFFICIENT_STORAGE
+                )
+            )
+            .build());
+    }
+
     @BeforeEach
     public void setUp() {
         wireMockServer = new WireMockServer(8888);
         wireMockServer.start();
-        botClient = new BotClient("http://localhost:" + wireMockServer.port());
+        botClient = new BotClient(retry, "http://localhost:" + wireMockServer.port());
         objectMapper = new ObjectMapper();
     }
 
@@ -55,5 +88,18 @@ public class BotClientTest {
         wireMockServer.verify(postRequestedFor(urlEqualTo("/updates"))
             .withHeader(HttpHeaders.CONTENT_TYPE, matching(MediaType.APPLICATION_JSON_VALUE))
             .withRequestBody(equalToJson(objectMapper.writeValueAsString(updateRequest))));
+    }
+
+    @SneakyThrows
+    @Test
+    public void testSend() {
+        Retry retryMock = Mockito.mock(Retry.class);
+
+        botClient = new BotClient(retryMock, "http://localhost:" + wireMockServer.port());
+
+
+        botClient.send(updateRequest);
+
+        Mockito.verify(retryMock).executeCallable(Mockito.any());
     }
 }
--- scrapper/src/test/java/edu/java/scrapper/api/StackoverflowClientTest.java
+++ scrapper/src/test/java/edu/java/scrapper/api/StackoverflowClientTest.java
@@ -7,11 +7,20 @@ import edu.java.clients.impl.StackoverflowClient;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import edu.java.clients.interfaces.WebClientStackoverflow;
+import edu.java.clients.retry.RetryConfigProxy;
+import edu.java.clients.retry.RetryPolicy;
+import edu.java.configuration.RetryConfiguration;
+import io.github.resilience4j.retry.Retry;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.util.ResourceUtils;
 import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
@@ -22,14 +31,36 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StackoverflowClientTest {
 
+    private static Retry retry;
+
     private WireMockServer server;
     private StackoverflowClient client;
+
+    @BeforeAll
+    static void beforeAll() {
+        retry = RetryConfiguration.start(RetryConfigProxy
+            .builder()
+            .policy(RetryPolicy.LINEAR)
+            .maxRetries(10)
+            .retryDelay(15L)
+            .increment(2)
+            .httpStatuses(
+                Arrays.asList(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    HttpStatus.SERVICE_UNAVAILABLE,
+                    HttpStatus.BAD_GATEWAY,
+                    HttpStatus.GATEWAY_TIMEOUT,
+                    HttpStatus.INSUFFICIENT_STORAGE
+                )
+            )
+            .build());
+    }
 
     @BeforeEach
     public void setUp() {
         server = new WireMockServer();
         server.start();
-        client = new StackoverflowClient("http://localhost:" + server.port());
+        client = new StackoverflowClient(retry,"http://localhost:" + server.port());
     }
 
     @AfterEach
@@ -67,6 +98,35 @@ public class StackoverflowClientTest {
     }
 
     @Test
+    public void testFetchStackOverflowQuestionWithRetry() throws IOException {
+        long questionId = 46125417;
+        File file = ResourceUtils.getFile("classpath:stackoverflow-success-response-data.json");
+        String expectedJson = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+
+        server
+            .stubFor(get("/questions/%d?site=stackoverflow".formatted(questionId))
+                .willReturn(aResponse()
+                    .withStatus(200)
+                    .withBody(expectedJson)
+                    .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)));
+
+        StackoverflowItemsResponse response = client.fetchStackOverflowQuestionRetry(questionId);
+
+        assertThat(response).isNotNull();
+        assertThat(response.items()).hasSize(1);
+
+        StackoverflowResponse question = response.items().get(0);
+        assertThat(question.id()).isEqualTo(46125417);
+        assertThat(question.link()).isEqualTo("https://stackoverflow.com/questions/46125417/how-to-mock-a-service-using-wiremock");
+        assertThat(question.owner()).isNotNull();
+        assertThat(question.owner().id()).isEqualTo(6181892);
+        assertThat(question.owner().login()).isEqualTo("worrynerd");
+        assertThat(question.lastActivityDate()).isNotNull();
+        assertThat(question.lastActivityDate().toEpochSecond()).isEqualTo(1505142513);
+
+    }
+
+    @Test
     public void testFetchStackOverflowQuestionWithInvalidUrl() throws IOException {
         long invalidQuestionId = 1;
 
@@ -81,6 +141,25 @@ public class StackoverflowClientTest {
                     .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)));
 
         assertThatThrownBy(() -> client.fetchStackOverflowQuestion(invalidQuestionId))
+            .isInstanceOf(RuntimeException.class)
+            .hasMessage("StackOverflow API Exception");
+    }
+
+    @Test
+    public void testFetchStackOverflowQuestionWithInvalidUrlWithRetry() throws IOException {
+        long invalidQuestionId = 1;
+
+        File file = ResourceUtils.getFile("classpath:stackoverflow-fail-response-data.json");
+        String expectedJson = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+
+        server
+            .stubFor(get("/questions/%d?site=stackoverflow".formatted(invalidQuestionId))
+                .willReturn(aResponse()
+                    .withStatus(404)
+                    .withBody(expectedJson)
+                    .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)));
+
+        assertThatThrownBy(() -> client.fetchStackOverflowQuestionRetry(invalidQuestionId))
             .isInstanceOf(RuntimeException.class)
             .hasMessage("StackOverflow API Exception");
     }
